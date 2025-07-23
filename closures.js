b26ec82a3ccd31f9461e2d5c8704e38b3834ce86
abhishek <abhishektiwariisro1278@gmail.com>
2025-07-23 17:07:40 +0530

    add call,apply,bind practice questions

---
+++ closures.js
@@ -0,0 +1,18 @@
+// 🔹 Problem 1: Simple Closure
+// function greetUser(name) {
+//   return function () {
+//     console.log(`Hi, ${name}`);
+//   };
+// }
+// const greet = greetUser("Abhi");
+// greet(); // ?
+
+
+// 🔹 Problem 2: Counter (as you did earlier)
+// function delayedLog() {
+//   let message = "I'm remembered!";
+//   setTimeout(function () {
+//     console.log(message);
+//   }, 1000);
+// }
+// delayedLog();
