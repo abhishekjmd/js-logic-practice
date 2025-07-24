afdcd825f28e622c6a9ee303bde7aa6b3a33d64f
abhishek <abhishektiwariisro1278@gmail.com>
2025-07-24 17:06:13 +0530

    add constructors

---
+++ constructors.js
@@ -0,0 +1,70 @@
+// 🔹 Q1: new keyword omitted
+// function Person(name) {
+//   this.name = name;
+// }
+// const p1 = new Person("Abhi");
+// console.log(p1); // ?
+
+
+// 🔹 Q2: new keyword used
+// function Person(name) {
+//   this.name = name;
+// }
+// const p1 = new Person("Abhi");
+// console.log(p1.name); // ?
+
+// 🔹 Q3: constructor returns object
+// function Car(make) {
+//   this.make = make;
+//   return { make: "Tesla" };
+// }
+// const c1 = new Car("Toyota");
+// console.log(c1.make); // ?
+
+
+// 🔹 Q4: constructor returns primitive
+// function Gadget(name) {
+//   this.name = name;
+//   return 42;
+// }
+// const g1 = new Gadget("Phone");
+// console.log(g1.name); // ?
+
+// 🔹 Q5: constructor hijack using Function
+// function Animal(type) {
+//   this.type = type;
+//   this.getType = function () {
+//     return this.type;
+//   }
+// }
+// const a = new Animal("Dog");
+// const b = new a.getType.constructor("return 'Cat'")();
+// console.log(b); // ?
+
+
+// 🔹 Q6: (Title: Nested new operators)
+// function Animal(type) {
+//   this.type = type;
+// }
+// Animal.prototype.sound = "Generic Sound";
+
+// const result = new new Animal("Dog").constructor("return 'Meow'");
+// console.log(result); // ?
+
+
+// 🔹 Q7: Chained new with constructor
+// function Person(name) {
+//   this.name = name;
+// }
+// const result = new new Person("John").constructor("return 7 * 6");
+// console.log(result); // ✅ 42
+
+// 🔹 Q8: Function returning object vs primitive
+// function Test() {
+//   this.name = "JS";
+//   return { topic: "Constructor" };
+// }
+// const t = new Test();
+// console.log(t.name); // ?
+
+
